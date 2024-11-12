fcaa27acebf2a2f1da7341f727957f21f3d60799
Ricardo Caldara Pinto <ricardo.caldara@uvvnet.com.br>
2024-11-11 22:35:10 -0300

    11/11/2024

---
+++ Aula11NOV24/javascript.js
@@ -0,0 +1,15 @@
+function butaun() {
+    var pegatexto = document.getElementById('textano').value;
+    if (pegatexto == "teste") {
+        window.alert("acertou, era: " + pegatexto);
+    }
+}
+
+var teste = window.prompt("teste0")
+window.confirm("teste1")
+
+if (teste == "foda") {
+    document.writeln("chama")
+} else {
+    document.writeln("sóferra")
+}
